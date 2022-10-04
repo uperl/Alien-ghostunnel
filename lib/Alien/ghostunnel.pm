5f81e9f529bbd8304e97d3d9fe6615214bae42da
Graham Ollis <plicease@cpan.org>
2022-10-04 06:44:39 -0600

    Initial structure

---
+++ lib/Alien/ghostunnel.pm
@@ -0,0 +1,13 @@
+package Alien::ghostunnel;
+
+use strict;
+use warnings;
+use 5.008004;
+use base qw( Alien::Base );
+
+# ABSTRACT: Find or install ghostunnel TLS proxy
+# VERSION
+
+1;
+
+
---
+++ t/alien_ghostunnel.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Alien::ghostunnel;
+
+ok 1, 'todo';
+
+done_testing;
+
+
